d5c260b3ba84a8f13ac0f80329ab4f4f35a8b502
MUHAMMAD-AHMAD-07 <not.roshaan@gmail.com>
2025-10-01 20:06:32 +0500

    Created the hero.js file for the landing-page.

--- src/components/hero.js
+++ src/components/hero.js
@@ -6,5 +6,19 @@ import ship2 from "../assets/images/ship2.png";
 
 export default function hero() {
     const getContainer = document.querySelector(".container");
+
     const getHero = createDiv(getContainer,"hero");
+
+    const getMenu = createDiv(getHero,"menu");
+    createPara(getMenu,"game-title","BattleShip")
+    const getMenuButtons = createDiv(getMenu,"menu-buttons");
+    createDiv(getMenuButtons,"start-btn");
+    createDiv(getMenuButtons,"instruction-btn");
+    createDiv(getMenuButtons,"setting-btn");
+
+
+    const getShipsContainer = createDiv(getHero,"ships-container");
+    createImage(getShipsContainer,ship1,"ship-one");
+    createImage(getShipsContainer,ship2,"ship-two");
+
 } 
