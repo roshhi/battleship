dd67b1ea05da5b03868a1c6c47a065ec90022745
MUHAMMAD-AHMAD-07 <not.roshaan@gmail.com>
2025-10-07 05:56:25 +0500

    Created and styled the player grid-board and back button on the main game screen.

--- src/components/main.js
+++ src/components/main.js
@@ -1,5 +1,41 @@
 import { createDiv, createPara, createSVG, createImage } from "../utils/utils.js";
+import back from "../assets/images/arrow.png";
 
 export default function main() {
     
+    const getContainer = document.querySelector(".container");
+    const getMainContent = createDiv(getContainer,"mainContent");
+    const getStartBtn = document.querySelector(".start-btn");
+    
+    const getBackBtn = createDiv(getMainContent,"main-back-btn");
+    const getMainBackBtn = createImage(getBackBtn,back,"main-back-img");
+
+    
+
+    getStartBtn.addEventListener("click",()=>{
+        getMainContent.classList.add("show");
+    })
+    getMainBackBtn.addEventListener("click",()=>{
+        document.querySelector(".menu").classList.remove("hide");
+        document.querySelector(".ships-container").classList.remove("hide");
+        getMainContent.classList.remove("show");
+    })
+
+    const getBoardsContainer = createDiv(getMainContent,"strategy-board-container"); 
+    const getPlayerBoard = createDiv(getBoardsContainer,"playerBoard");
+    const rows = 10;
+    const cols = 10;
+
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < cols; c++) {
+            const cell = document.createElement("div");
+            cell.classList.add("cell");
+            // cell.dataset.row = r;
+            // cell.dataset.col = c;
+            getPlayerBoard.appendChild(cell); // add cell to board
+        }
+    }
+
+    const getShipSelectionBorad = createDiv(getBoardsContainer,"ships-selection");
+
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import "./styles.css";
 import landingPage from "./pages/landing-page";
+import main from "./components/main";
 
 landingPage();
+main();
